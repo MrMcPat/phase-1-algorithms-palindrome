1f21b18fc85700a7febdfa65e5a10bbfa1ef01c5
Patrick Liang <pl204511194@yahoo.com>
2022-01-21 13:42:14 -0500

    commit tree 0601899e5c0bd939471677675df1273ad63c4628

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  wordArray = word.split("");
+  let reversedArray = [];
+  
+  for(let i=wordArray.length; i>=0; i--) {
+      reversedArray.push(wordArray[i]);
+  }
+
+  let reversedString = reversedArray.join("");
+  return reversedString === word ? true : false;
+
 }
 
 /* 
   Add your pseudocode here
+  Turn the word into an array using split.
+  Reverse the elements in the array with a for loop.
+  Declare a new empty array to push the reversed elements into it.
+  Convert that array into a string and compare the word and the reversed string.
 */
 
 /*
   Add written explanation of your solution here
+  wordArray = word.split("") is used to convert the word to an array
+  let reversedArray = [] is used so the reversed letters can be pushed into this array
+
+  The for loop is used to push the reversed elements to the reversedArray
+
+  let reveresedString = reversedArray.join("") is used to convert the array into a string
+
+  And the ternary operator is used to return true if reversedString and word are equal
+
 */
 
 // You can run `node index.js` to view these console logs
